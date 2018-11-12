514870d7c342e90c17077c8ff938f2790c46ff94
Igor-Nosatov <sinevor13@gmail.com>
2018-11-12 13:25:23 +0200

    counting operation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,8 +17,20 @@ class ProductController extends Controller
     {
         $products = Product::query()->findOrFail($id);
         $reviews = ProductReviews::query()->latest()->with('product')->paginate(3);
+
+        $count_star = ProductReviews::query()->sum('rating');
         $avg_stars = ProductReviews::query()->avg('rating');
-        return view('pages.product',compact(['products','reviews','avg_stars']));
+        $star5= ProductReviews::query()->where('rating','=','5');
+        $star4= ProductReviews::query()->where('rating','=','4');
+        $star3= ProductReviews::query()->where('rating','=','3');
+        $star2= ProductReviews::query()->where('rating','=','2');
+        $star1= ProductReviews::query()->where('rating','=','1');
+
+
+        return view('pages.product',compact
+        	(['products','reviews','avg_stars',
+        		'star5','star4','star3',
+        		'star2','star1','count_star']));
     }
 
 }
