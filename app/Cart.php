002e1f4956e07e0ba64b551aa8765e2d225076fe
Igor-Nosatov <sinevor13@gmail.com>
2018-11-13 15:23:46 +0200

    add structure of CartModel

--- app/Cart.php
+++ app/Cart.php
@@ -2,9 +2,36 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Product;
+use App;
 
 class Cart extends Model
 {
-    //
+    public $id;
+    public $name;
+    public $price;
+    public $description;
+    public $category;
+    public $brand;
+    public $color;
+    public $size;
+    
+
+    public $quantityCart = 0;
+
+    public static function cart_product(Product $product)
+    {
+       $cartProduct = new CartProduct;
+       
+       $cartProduct->id = $product->id;
+       $cartProduct->name = $product->name;
+       $cartProduct->price = $product->price;
+       $cartProduct->image =  $product->image;
+       $cartProduct->category = $product->category->name;
+       $cartProduct->brand = $product->brand->name;
+       $cartProduct->color = $product->color->name;
+       $cartProduct->size = $product->size->name;
+       
+        return $cartProduct;
+    }
 }
--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,8 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App; 
+use App\Product;
+use App\Cart;
+use Session;
+
+
 
 class CartController extends Controller
 {
-    //
+     public function index(){
+        $products = session()->get('pages.cart');
+
+        $total = 0;
+        $counter = 0;
+
+        
+        
+        if($products != null){
+            foreach ($products as $product)
+                $counter += $product->quantityCart;
+            $total += CartController::totalCost($products);
+        }
+        
+        if($counter >= 10)
+            $total = $total * 0.9;
+
+        return view('pages.cart', compact(['products','total']));
+    }
+
+
+     public function stored(Request $request) {
+
+        $id = $request['product_id'];
+        
+        if($request->session()->has('pages.cart')){
+            foreach($request->session()->get('pages.cart') as $product) {
+                if($id == $product->id) {
+                    $request['type'] = 'plus';
+                    return $this->editProduct($request,  $product->id);
+                }
+            }
+        }
+
+        $cartProduct = CartProduct::fromProduct(Product::get()->where('id', $id)->first());
+        
+        $cartProduct->quantityCart++;
+        
+        $request->session()->push('pages', $cartDarkBeer);
+
+        return redirect('/'.App::getLocale().'/cart');
+    }
 }
