c9e255a36d1ed851cc548f4e1afe0947b6d7e011
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:12:21 +0300

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -1,44 +1,35 @@
 <?php
-
 namespace App;
-
 use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
-{ 
-  use Searchable;
-
-  protected $table = 'products';
-
-  protected $fillable = ['name','description', 'image', 'price'];
-
-  public function category() 
-  {
-    return $this->belongsTo('App\Category', 'category_id');
-  }
-  public function brand() 
-  {
-    return $this->belongsTo('App\Brand', 'brand_id');
-  }
-  public function review() 
-  {
-    return $this->hasMany('App\ProductReviews', 'review_id');
-  }
-
-  public function cart() 
-  {
-    return $this->hasMany('App\Cart');
-  }
-
-  public function color()
-  {
-    return $this->belongsToMany('App\Color');
-  }
-
-  public function size()
-  {
-    return $this->belongsToMany('App\Size');
-  }
+class Product extends Model {
+    use Searchable;
+    protected $table = 'products';
+    protected $fillable = ['name', 'description', 'image', 'price'];
     
+    public function category() 
+    {
+        return $this->belongsTo('App\Category', 'category_id');
+    }
+    public function brand()
+    {
+        return $this->belongsTo('App\Brand', 'brand_id');
+    }
+    public function review() 
+    {
+        return $this->hasMany('App\ProductReviews', 'review_id');
+    }
+    public function cart() 
+    {
+        return $this->hasMany('App\Cart');
+    }
+    public function color() 
+    {
+        return $this->belongsToMany('App\Color');
+    }
+    public function size() 
+    {
+        return $this->belongsToMany('App\Size');
+    }
 }
