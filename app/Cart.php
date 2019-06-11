054997e0527d2aeb0e6a01b6cc5125337903539d
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:09:09 +0300

    Update Cart.php

--- app/Cart.php
+++ app/Cart.php
@@ -1,22 +1,17 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
-class Cart extends Model
+class Cart extends Model 
 {
-    
     protected $table = "cart";
-
-    protected $fillable = ['products_id',
-      'name','image','price','size','color','number'];
-
-    public function product()
-    {
-      return $this->belongsToMany('App\Product');
-    }
+    protected $fillable = ['products_id', 'name', 'image', 'price', 'size', 'color', 'number'];
     
+    public function product() 
+    {
+        return $this->belongsToMany('App\Product');
+    }
 }
+
 
 
