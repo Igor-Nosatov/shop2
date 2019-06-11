336568081a2ab350fe3898ea875c1f543023d7dc
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:14:31 +0300

    Update CheckoutController.php

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -1,57 +1,32 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Cart;
 use App\Checkout;
 
-class CheckoutController extends Controller
-{
-    public function index()
-    {   
-        
+class CheckoutController extends Controller {
+    
+    public function index() 
+    {
         $products = Cart::query()->orderBy('name', 'desc')->get();
-       $total_price = Cart::query()->sum('price');
-          $total_item = Cart::query()->sum('number');
-          $cart_item = Cart::query()->get();
-        
-
-    	return view('pages.checkout', compact(['products','total_price','total_item','cart_item']));
+        $total_price = Cart::query()->sum('price');
+        $total_item = Cart::query()->sum('number');
+        $cart_item = Cart::query()->get();
+        return view('pages.checkout', compact(['products', 'total_price', 'total_item', 'cart_item']));
     }
-
-    public function store(Request $request) { 
-
-        $this->validate($request, [
-     
-            'first_name'  => 'required',
-            'last_name'   => 'required',
-            'email'       => 'required',
-            'address'     => 'required',
-            'city'        => 'required',
-            'country'     => 'required',
-            'zip_code'    => 'required',
-            'telephone'   => 'required',
-            'total_price' => 'required'
-        ]);
-
-            $first_name  = $request['first_name'];
-            $last_name   = $request['last_name'];
-            $email       = $request['email'];
-            $address     = $request['address'];
-            $city        = $request['city'];
-            $country     = $request['country'];
-            $zip_code    = $request['zip_code'];
-            $telephone   = $request['telephone '];
-            $total_price = $request['total_price'];
-
-        $checkout_model = Checkout::create(
-           [  'first_name' => $first_name, 'last_name' => $last_name, 
-           'email' =>$email,'address' =>$address, 'city' =>$city,
-           'country' => $country, 'zip_code' =>$zip_code,'telephone' =>$telephone, 
-           'total_price' =>$total_price,]
-        );
-
+    public function store(Request $request) 
+    {
+        $this->validate($request, ['first_name' => 'required', 'last_name' => 'required', 'email' => 'required', 'address' => 'required', 'city' => 'required', 'country' => 'required', 'zip_code' => 'required', 'telephone' => 'required', 'total_price' => 'required']);
+        $first_name = $request['first_name'];
+        $last_name = $request['last_name'];
+        $email = $request['email'];
+        $address = $request['address'];
+        $city = $request['city'];
+        $country = $request['country'];
+        $zip_code = $request['zip_code'];
+        $telephone = $request['telephone '];
+        $total_price = $request['total_price'];
+        $checkout_model = Checkout::create(['first_name' => $first_name, 'last_name' => $last_name, 'email' => $email, 'address' => $address, 'city' => $city, 'country' => $country, 'zip_code' => $zip_code, 'telephone' => $telephone, 'total_price' => $total_price, ]);
         return redirect()->back();
     }
 }
