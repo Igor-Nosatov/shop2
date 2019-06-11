7fa6342335caa3d238d34cebd984b3b02412a257
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:10:32 +0300

    Update Checkout.php

--- app/Checkout.php
+++ app/Checkout.php
@@ -1,14 +1,9 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
-class Checkout extends Model
+class Checkout extends Model 
 {
     protected $table = 'checkout';
-
-    protected $fillable = ['first_name','last_name','email',
-
-        'adress','city','country','zip_code','telephone'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'adress', 'city', 'country', 'zip_code', 'telephone'];
 }
